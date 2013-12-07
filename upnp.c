b7c5ce09742626cfa3630a5a3840ca28a81d648b
Michal Marek <michal@markovi.net>
2013-12-07 21:57:00 +0100

    Use pthread conditional variables to synchronize the UPnP handshake
    The handshake is not complete yet.

--- upnp.c
+++ upnp.c
@@ -1,8 +1,11 @@
 #include <sys/stat.h>
 #include <string.h>
+#include <errno.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "upnptools.h"
+#include "ithread.h"
 #include "upnp.h"
 
 #include "wphoto.h"
@@ -12,17 +15,26 @@ int server_port;
 static UpnpDevice_Handle device_handle = -1;
 static UpnpClient_Handle client_handle = -1;
 
+/*
+ * This is used to signal the main thread of stage changes of the device
+ * or client. The mutex protects all the three flag variables.
+ */
+ithread_mutex_t state_mutex;
+ithread_cond_t state_cond;
+/* client discovery found a camera */
+int camera_found;
+/* camera issued a GET request to our device */
+int camera_responded;
+int discovery_timeout;
+
+#define ADVERTISEMENT_INTERVAL 3
+#define MSEARCH_INTERVAL 5
+#define CAMERA_SERVICE_NAME "urn:schemas-canon-com:service:MobileConnectedCameraService:1"
+
 static void upnp_perror(const char *message, int err)
 {
 	fprintf(stderr, "%s: %s (%d)\n", message, UpnpGetErrorMessage(err), err);
 };
-
-enum discovery_state {
-	DISCOVERY_NOTSTARTED,
-	DISCOVERY_RUNNING,
-	DISCOVERY_TIMEOUT,
-	DISCOVERY_FOUND,
-} device_dstate, client_dstate;
 
 static int upnp_device_event_handler(Upnp_EventType type, void *event,
 		void *cookie)
@@ -52,7 +64,10 @@ static int upnp_client_event_handler(Upnp_EventType type, void *event,
 
 	switch (type) {
 	case UPNP_DISCOVERY_SEARCH_TIMEOUT:
-		client_dstate = DISCOVERY_TIMEOUT;
+		ithread_mutex_lock(&state_mutex);
+		discovery_timeout = 1;
+		ithread_cond_signal(&state_cond);
+		ithread_mutex_unlock(&state_mutex);
 		break;
 	case UPNP_DISCOVERY_ADVERTISEMENT_ALIVE:
 	case UPNP_DISCOVERY_ADVERTISEMENT_BYEBYE:
@@ -92,7 +107,13 @@ int wphoto_upnp_handshake(void)
 	int ret = -1, err;
 	char descurl[256];
 	const char *desc_xml = "MobileDevDesc.xml";
+	struct timespec timer;
+	int camera_found_save, camera_responded_save;
 
+	ithread_mutex_init(&state_mutex, NULL);
+	ithread_cond_init(&state_cond, NULL);
+	camera_found = 0;
+	camera_responded = 0;
 	err = UpnpInit(NULL, 0);
 	if (err != UPNP_E_SUCCESS) {
 		printf("UpnpInit error: %d\n", err);
@@ -137,7 +158,12 @@ int wphoto_upnp_handshake(void)
 		printf("UpnpRegisterClient error: %d\n", err);
 		goto err_register;
 	}
+	clock_gettime(CLOCK_REALTIME, &timer);
+	discovery_timeout = 1;
+	camera_responded_save = 0;
+	camera_found_save = 0;
 	do {
+		int wait_err;
 		MARK("/x1");
 		err = UpnpSendAdvertisement(device_handle, 0);
 		MARK("/x2");
@@ -145,27 +171,33 @@ int wphoto_upnp_handshake(void)
 			printf("UpnpSendAdvertisement error: %d\n", err);
 			goto err_register;
 		}
-		device_dstate = DISCOVERY_RUNNING;
 		printf("NOTIFY sent\n");
-		switch (client_dstate) {
-		case DISCOVERY_RUNNING:
-		case DISCOVERY_FOUND:
-			sleep(3);
-			continue;
-		case DISCOVERY_TIMEOUT:
-			printf("M-SEARCH timeout\n");
-		default:
-			;
+		timer.tv_sec += ADVERTISEMENT_INTERVAL;
+wait:
+		ithread_mutex_lock(&state_mutex);
+		wait_err = 0;
+		while (camera_responded == camera_responded_save &&
+				camera_found == camera_found_save &&
+				!discovery_timeout && wait_err == 0)
+			wait_err = ithread_cond_timedwait(
+					&state_cond, &state_mutex, &timer);
+		if (discovery_timeout) {
+			err = UpnpSearchAsync(client_handle, MSEARCH_INTERVAL,
+					CAMERA_SERVICE_NAME, (void*)42);
+			if (err != UPNP_E_SUCCESS) {
+				printf("UpnpSearchAsync error: %d\n", err);
+				goto err_register;
+			}
+			printf("M-SEARCH sent\n");
+			discovery_timeout = 0;
 		}
-		client_dstate = DISCOVERY_RUNNING;
-		err = UpnpSearchAsync(client_handle, 5, "urn:schemas-canon-com:service:MobileConnectedCameraService:1", (void*)42);
-		if (err != UPNP_E_SUCCESS) {
-			printf("UpnpSearchAsync error: %d\n", err);
-			goto err_register;
-		}
-		printf("M-SEARCH sent\n");
-		sleep(3);
-	} while (device_dstate != DISCOVERY_FOUND && client_dstate != DISCOVERY_FOUND);
+		camera_responded_save = camera_responded;
+		camera_found_save = camera_found;
+		ithread_mutex_unlock(&state_mutex);
+		if (wait_err != ETIMEDOUT &&
+				(!camera_found_save || !camera_responded_save))
+			goto wait;
+	} while (!camera_found_save || !camera_responded_save);
 	ret = 0;
 err_register:
 	UpnpUnRegisterRootDevice(device_handle);
