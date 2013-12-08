bbb7939b0f81377cd83774a9298b01fdee7e46d2
Michal Marek <michal@markovi.net>
2013-12-08 16:14:00 +0100

    Do not call UpnpFinish() twice in error path

--- web.c
+++ web.c
@@ -130,13 +130,11 @@ int web_start(void)
 	ret = UpnpEnableWebserver(TRUE);
 	if (ret != UPNP_E_SUCCESS) {
 		printf("UpnpEnableWebserver error: %d\n", ret);
-		UpnpFinish();
 		return 1;
 	}
 	ret = UpnpAddVirtualDir("/");
 	if (ret != UPNP_E_SUCCESS) {
 		printf("UpnpAddVirtualDir error: %d\n", ret);
-		UpnpFinish();
 		return 1;
 	}
 	UpnpVirtualDir_set_GetInfoCallback(upnp_web_getinfo);
